7d31d9577361f28ed7d1dcea7f8d56c90976bd78
Tim L <tclenz@gmail.com>
2022-10-25 10:58:08 -0400

    Update ReactiveWebSocketHandler.java

--- ReactiveWebSocketHandler.java
+++ ReactiveWebSocketHandler.java
@@ -1,3 +1,5 @@
+package birdr.life.ws;
+
 import java.time.Duration;
 import java.util.Collection;
 import java.util.List;
@@ -5,6 +7,7 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nullable;
 import javax.annotation.PostConstruct;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
@@ -14,6 +17,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -35,15 +39,22 @@ import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import com.google.firebase.messaging.Notification;
 
+import birdr.life.birder.BirderRepo;
+import birdr.life.firebase.FirebasePushService;
+import birdr.life.message.ChatRepo;
+import birdr.life.message.Message;
+import birdr.life.message.MessageRepo;
+import birdr.life.message.MessageSavedEvent;
+import birdr.life.security.UserService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.core.publisher.Sinks.Many;
 
 @Component
-public class ReactiveWebSocketHandler implements WebSocketHandler 
+public class ReactiveWebSocketHandler implements WebSocketHandler, ApplicationListener<MessageSavedEvent>
 {	
-    Logger logger = LoggerFactory.getLogger(ReactiveWebSocketHandler.class);
+	Logger logger = LoggerFactory.getLogger(ReactiveWebSocketHandler.class);
 	
     private ObjectMapper objectMapper = JsonMapper.builder().serializationInclusion(Include.NON_NULL).build();
     
@@ -56,7 +67,10 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
     private FirebasePushService firebasePushService;
     
     @Autowired
-    private UserRepo userRepo;
+    private BirderRepo birderRepo;
+    
+    @Autowired
+    private ChatRepo chatRepo;
 	
     @PostConstruct
     private void init()
@@ -64,11 +78,16 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
     	objectMapper.registerModule(new JavaTimeModule());
     }
     
-    public static void updateTokenCache(String username,String token)
+    public static void updateTokenCache(String username,@Nullable String token)
     {
     	String curToken = firebaseTokenCache.getIfPresent(username);
     	if (curToken != null && !curToken.equals(token))
-    		firebaseTokenCache.put(username,token);
+    	{
+    		if (token == null)
+    			firebaseTokenCache.invalidate(username);
+    		else
+    			firebaseTokenCache.put(username,token);
+    	}
     }
     
 	@Autowired
@@ -77,18 +96,18 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
 	@Autowired
 	private Validator validator;
 	
-	private static final Multimap<String,Pair<String,Many<Object>>> topicSessionIdMap = Multimaps.synchronizedMultimap(ArrayListMultimap.create());	
+	private static final Multimap<String,Pair<String,Many<WebSocketResponse>>> topicSessionIdMap = Multimaps.synchronizedMultimap(ArrayListMultimap.create());	
 	
 	/**
 	 * @param topic
 	 * @param res
 	 * @return true if an active websocket session exists for this topic. The event may or may not be emitted successfully
 	 */
-	public static boolean emitEvent(String topic,WebSocketResponse res)
+	public static Mono<Boolean> emitEvent(String topic,WebSocketResponse res)
 	{
-		Collection<Pair<String, Many<Object>>> col = topicSessionIdMap.get(topic);
+		Collection<Pair<String, Many<WebSocketResponse>>> col = topicSessionIdMap.get(topic);
 		if (col.isEmpty())
-			return false;
+			return Mono.empty();
 		
 		col.stream().forEach(p ->
 		{
@@ -96,37 +115,39 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
 			p.getRight().emitNext(res,Sinks.EmitFailureHandler.FAIL_FAST);
 		});
 		
-		return true;
+		return Mono.just(true);
 	}
 	
-	private Mono<Void> emitFirebaseNotif(String toUsername,Message msg)
+	private Mono<Boolean> emitFirebaseNotif(String toUsername,Message msg)
 	{
+		logger.debug("Active websocket connection for " + toUsername + " not found, trying firebase notif...");
+
 		Mono<String> tokenMono;
 		String cachedToken = firebaseTokenCache.getIfPresent(toUsername);
 		if (cachedToken != null)
 			tokenMono = Mono.just(cachedToken);
 		else
-			tokenMono = userRepo.loadFirebaseToken(toUsername).doOnNext(token -> firebaseTokenCache.put(toUsername,token));
+			tokenMono = birderRepo.loadFirebaseToken(toUsername).doOnNext(token -> firebaseTokenCache.put(toUsername,token));
 		
 		return tokenMono.flatMap(token ->
 		{
-			return userRepo.miniProfile(msg.getFromUsername()).flatMap(fromUser -> 
+			return birderRepo.miniProfile(msg.getFromUsername()).flatMap(fromUser -> 
 			{
 				String title = "Convo with " + fromUser.getFirstName();
 				String body = "@" + msg.getFromUsername() + ": " + msg.getText();
 				
 				Notification notif = Notification.builder().setTitle(title).setBody(body).build();	
 				logger.debug("Sending firebase notif to " + toUsername);
-				return firebasePushService.send(token,notif).then();
+				return firebasePushService.send(token,notif).then(Mono.just(true));
 			});
 		});	
 	}
 	
-	private WebSocketMessage objToWebSocketMsg(Object obj,WebSocketSession webSocketSession)
+	private WebSocketMessage objToWebSocketMsg(WebSocketResponse res,WebSocketSession webSocketSession)
 	{
 		String text = null;
 		try {
-			text = objectMapper.writeValueAsString(obj);
+			text = objectMapper.writeValueAsString(res);
 		} catch (JsonProcessingException e) 
 		{
 			logger.error("Cannot convert obj to str",e);
@@ -134,36 +155,50 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
 		}
 		
 		return webSocketSession.textMessage(text);
-	}	
+	}		
 	
-	public Mono<Void> handleSendMessage(String toUsername,Message msg)
+	private Mono<Boolean> emitEventOrFirebase(String username,WebSocketResponse res) 
 	{
-		WebSocketResponse sucRes = new WebSocketResponse(HttpStatus.CREATED.value(),"send-message");
-		String username = msg.getFromUsername();
-		sucRes.setMessageId(msg.getMessageId());
-		sucRes.setDestId(toUsername);
-		
-		emitEvent(username,sucRes);
-		
-		sucRes.setData(msg);
-		
-		if (!emitEvent(toUsername,sucRes))
-		{
-			logger.debug("Active websocket connection for " + toUsername + " not found, trying firebase notif...");
-			return emitFirebaseNotif(toUsername,msg);
-		}
-		
-		return Mono.empty();
+		return emitEvent(username,res)
+			   .switchIfEmpty(emitFirebaseNotif(username,(Message)res.getData()));
 	}
 	
-	public static Flux<String> handleChatMessage(ChatRepo chatRepo,String chatId,Message msg)
+	@Override
+	public void onApplicationEvent(MessageSavedEvent event) 
 	{
-		WebSocketResponse sucRes = new WebSocketResponse(HttpStatus.CREATED.value(),"chat-message");
+		String destId = event.getDestId();
+		boolean isChat = destId.startsWith("CHAT");
+		String command = isChat ? "chat-message" : "send-message";
+		Message msg = (Message)event.getSource();
+		
+		WebSocketResponse sucRes = new WebSocketResponse(HttpStatus.CREATED.value(),command);
 		sucRes.setMessageId(msg.getMessageId());
-		sucRes.setDestId(chatId);
+		sucRes.setDestId(destId);
+		
+		emitEvent(msg.getFromUsername(),sucRes);		
 		sucRes.setData(msg);
 		
-		return chatRepo.members(chatId).doOnNext(username -> emitEvent(username,sucRes));
+		if (isChat)
+			chatRepo.members(destId).flatMap(username -> emitEventOrFirebase(username,sucRes).onErrorComplete()).blockLast();
+		else
+			emitEventOrFirebase(destId,sucRes).block();
+	}
+	
+	private boolean validateMessage(WebSocketCommand wsc,String fromUsername)
+	{
+		Message message = wsc.getMessage();
+		Set<ConstraintViolation<Message>> errors = validator.validate(message);
+		if (!errors.isEmpty())
+		{
+			List<String> errorList = errors.stream().map(cv -> cv.getMessage()).collect(Collectors.toList());
+			WebSocketResponse errRes = new WebSocketResponse(HttpStatus.BAD_REQUEST.value(),wsc.getCommand());
+			errRes.setFrame(wsc);
+			errRes.setErrors(errorList);
+			emitEvent(fromUsername,errRes);
+			return false;
+		}
+		
+		return true;
 	}
 
     @Override
@@ -178,10 +213,10 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
 				StopWatch pingPongTimer = StopWatch.create();
 
 				String sessionId = webSocketSession.getId();
-				Many<Object> generator = Sinks.many().unicast().onBackpressureBuffer();	
+				Many<WebSocketResponse> generator = Sinks.many().unicast().onBackpressureBuffer();	
 				topicSessionIdMap.put(username,Pair.of(sessionId,generator));
 				
-				Flux<WebSocketMessage> eventFlux = generator.asFlux().map(obj -> objToWebSocketMsg(obj,webSocketSession));				
+				Flux<WebSocketMessage> eventFlux = generator.asFlux().map(res -> objToWebSocketMsg(res,webSocketSession));				
 				Flux<WebSocketMessage> pingFlux = Flux.interval(Duration.ofSeconds(PING_INTERVAL_SEC)).map(l ->
 				{
 					if (pingPongTimer.getTime(TimeUnit.SECONDS) > PING_PONG_TIMEOUT_SEC)
@@ -218,23 +253,19 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
 						return Mono.error(e);
 					}
 					
+					if (!validateMessage(wsc,username))
+						return Mono.empty();
+					
 					if ("send-message".equals(wsc.getCommand()))
 					{
 						String toUsername = wsc.getUsername();
-						Message message = wsc.getMessage();
-						
-						Set<ConstraintViolation<Message>> errors = validator.validate(message);
-						if (!errors.isEmpty())
-						{
-							List<String> errorList = errors.stream().map(cv -> cv.getMessage()).collect(Collectors.toList());
-							WebSocketResponse errRes = new WebSocketResponse(HttpStatus.BAD_REQUEST.value(),wsc.getCommand());
-							errRes.setFrame(wsc);
-							errRes.setErrors(errorList);
-							emitEvent(username,errRes);
-							return Mono.empty();	
-						}
-						
-						return messageRepo.save(toUsername,message).flatMap(msg -> handleSendMessage(toUsername,msg));
+						return messageRepo.save(toUsername,wsc.getMessage());
+					}
+					else if ("chat-message".equals(wsc.getCommand()))
+					{
+						String chatId = wsc.getChatId();
+						Message message = wsc.getMessage();						
+						return chatRepo.sendChatMsg(chatId,message);
 					}
 					
 					return Mono.empty();
@@ -247,4 +278,5 @@ public class ReactiveWebSocketHandler implements WebSocketHandler
 		        });
 			}).switchIfEmpty(webSocketSession.close(CloseStatus.POLICY_VIOLATION));
     }
+	
 };
